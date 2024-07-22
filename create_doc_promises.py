0bd26cec454477f457ff85acec4c326dc3c85407
wolf <wolf@wolfbian.168.1.6>
2024-07-21 18:38:57 -0600

    UPDATE create doc promises description

--- create_doc_promises.py
+++ create_doc_promises.py
@@ -1,24 +1,6 @@
 import os
 from docxtpl import DocxTemplate
 
-dates_promises = ['5 de agosto de 2024', '5 de septiembre de 2024', '7 de octubre de 2024', '5 de noviembre de 2024', '5 de diciembre de 2024', '6 de enero de 2025', '6 de febrero de 2025', '5 de marzo de 2025', '7 de abril de 2025', '6 de mayo de 2025', '5 de junio de 2025']
-
-data = {
-        "counter": 0,
-        "amount_rent": "5,000.00",
-        "signature_date" : "5 de agosto de 2024",
-        "promises": 0,
-        "date_promise": "",
-        "lessor_name": "Chancito Feliz",
-        "lessor_address": "La Michocana 135, Metepec, Mexico.",
-        "amount_rent_letter": "Cinco mil",
-        "tenant_name": "Chanchito Juarez",
-        "jointly_obligated_name": "Obligado Chanchito Gasrcia",
-        "tenant_address": "Quinta Avenida 50, Toluca, México.",
-        "jointly_obligated_address": "Quinta Avenida 50, Toluca, México.",
-        "municipality": "Toluca"
-        }
-
 def create_pay_promises_docx(template_path, data, dates_promises):
     # DEFNIR UN CONTADOR
     counter = 0
